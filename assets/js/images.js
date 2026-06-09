253f18a60cf624fb62e6b470525e8d154ae7edc9
JamesBrownR <jamesbrown1real@gmail.com>
2026-06-09 19:24:12 -0400

    Create images.js

---
+++ assets/js/images.js
@@ -0,0 +1,27 @@
+// GraviTclean Gallery Images
+// To add a new image: drop the file in the /images/ folder,
+// then add a new entry to this array following the same format.
+// caption is optional but helps with accessibility.
+
+const GALLERY_IMAGES = [
+  {
+    src: "images/Gravitclean2.webp",
+    caption: "Aerial drone view of a residential roof cleaning in Miami"
+  },
+  {
+    src: "images/Gravitclean4.webp",
+    caption: "GraviTclean crew and equipment on site in a Miami neighborhood"
+  },
+  {
+    src: "images/Gravitclean5.webp",
+    caption: "Paver patio cleaning results"
+  },
+  {
+    src: "images/Gravitclean6.webp",
+    caption: "Commercial building exterior washing"
+  },
+  {
+    src: "images/Gravitclean8.webp",
+    caption: "GraviTclean drone ready for a rooftop clean"
+  }
+];
